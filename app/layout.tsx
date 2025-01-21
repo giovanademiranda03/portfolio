c9da455cd151db98129348a23d8deb51fc78c555
giovanademiranda03 <giovanademirandagithub@gmail.com>
2025-01-21 14:51:17 -0300

    fix: avoid exporting pages with server-side rendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,16 @@ import { ThemeProvider } from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export async function getStaticProps() {
+  const res = await fetch("https://giovana-portfolio.vercel.app");
+  const data = await res.json();
+
+  return {
+    props: { data },
+    revalidate: 60,
+  };
+}
+
 export const metadata: Metadata = {
   title: "Giovana de Miranda Portfolio",
   description: "My Personal Portfolio",

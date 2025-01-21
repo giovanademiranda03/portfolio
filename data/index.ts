05f2c14848d2e3473e6dfe4731eae2b1cff5c2fe
giovanademiranda03 <giovanademirandagithub@gmail.com>
2025-01-21 04:45:27 -0300

    feat: add folder data

---
+++ data/index.ts
@@ -0,0 +1,139 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+  { name: "LinkedIn", link: "https://www.linkedin.com/in/giovana-de-miranda" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Systems analysis to deliver exceptional user experiences",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Flexible communication across time zones",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: " text-nowrap lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Passionate about responsive design and UI/UX",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+  {
+    id: 5,
+    title: "Developing innovative solutions: currently a JS animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Let’s bring your vision to life. Start your project with me!",
+    description: "",
+    className: "lg:col-span-5 md:col-span-6 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "CNPJws",
+    des: "In this project, I worked on UI/UX design, development, responsive interfaces, and system analysis.",
+    img: "/cnpj.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/intl.png", "/figma.png"],
+    link: "https://www.cnpj.ws",
+  },
+  {
+    id: 2,
+    title: "PipeOrder",
+    des: "In this project, I worked on UI/UX design, development, responsive interfaces, and system analysis.",
+    img: "/pipeorder.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/intl.png", "/ac.png"],
+    link: "https://www.pipeorder.io",
+  },
+  {
+    id: 3,
+    title: "Bitize (Company Landing Page)",
+    des: "In this project, I worked on UI/UX design, development, responsive interfaces, and system analysis.",
+    img: "/bitize.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/intl.png", "/figma.png"],
+    link: "https://www.bitize.com.br",
+  },
+  {
+    id: 4,
+    title: "Dados de Empresas",
+    des: "In this project, I worked on UI/UX design, system analysis and development",
+    img: "/dadosdeempresas.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/figma.png"],
+    link: "https://www.dadosdeempresas.com",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "System Analysis Jr",
+    desc: "Development of systems using Next.js, Tailwind CSS, and TypeScript. Interface responsiveness. Prototyping with the Figma tool for interface design, UX Design studies for projects, creation of Design Systems, and development of color palettes for projects. Implementation of automated E2E tests using Cypress. Responsible for analyzing functional and non-functional requirements, creating user stories, and developing the Front-End logical design.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Systems Development Intern",
+    desc: "Systems development training with Node, React, React Native and Next. Interface Prototyping Training Assistance in updating systems development. Project documentation",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/giovanademiranda03/",
+  },
+  {
+    id: 2,
+    img: "/ig.svg",
+    link: "https://www.instagram.com/gioymir/",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/giovana-de-miranda/",
+  },
+];

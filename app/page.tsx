e3afee31d671038bf8ee69547edf1ccc09f16f19
giovanademiranda03 <giovanademirandagithub@gmail.com>
2025-01-21 16:26:28 -0300

    refactor: add suspense in page

--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,21 @@ import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { navItems } from "@/data";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={navItems} />
-        <Hero />
-        <Grid />
-        {/* <RecentProjects />
-        <Experience />
-        <Approach />
-        <Footer /> */}
+        <Suspense fallback={<>Loading...</>}>
+          <FloatingNav navItems={navItems} />
+          <Hero />
+          <Grid />
+          <RecentProjects />
+          <Experience />
+          <Approach />
+          <Footer />
+        </Suspense>
       </div>
     </main>
   );

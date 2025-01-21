eb433ad99ad6e52e7b7916262a127975edd6231c
giovanademiranda03 <giovanademirandagithub@gmail.com>
2025-01-21 16:38:00 -0300

    feat: add missingSuspenseWithCSRBailout on next config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 module.exports = nextConfig;
